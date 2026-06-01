f93a48917de62f520418ae2754ea255352225457
PuruMohite <107110840+PuruMohite@users.noreply.github.com>
2026-06-01 14:31:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+let table = document.getElementById("sampleTable");
 function insert_Row() {
     //Write your code here
-  
+    table.innerHTML = `<tr><td>New Cell1</td> 
+		<td>New Cell2</td></tr>` + table.innerHTML;
   
 }
